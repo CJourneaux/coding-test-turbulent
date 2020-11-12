c35b230d6057d81b528158e72580f8c3aa047ec2
Cécile Journeaux <cecile.journeaux@gmail.com>
2020-11-12 01:57:46 +0000

    :lipstick: Adding the Chakra theme to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+      <CSSReset />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
