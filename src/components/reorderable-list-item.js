1a5951f3018b603c2331df809d0152b366712f52
Cécile Journeaux <cecile.journeaux@gmail.com>
2020-11-16 21:54:13 +0000

    :label: :memo: Adding types to sub-components

--- src/components/reorderable-list-item.js
+++ src/components/reorderable-list-item.js
@@ -1,13 +1,9 @@
 import * as React from "react";
 import { ListItem, ListIcon } from "@chakra-ui/react";
 import { MdReorder } from "react-icons/md";
+import PropTypes from "prop-types";
 
-export const ReorderableListItem = ({
-  value,
-  isDragged,
-  isSelected,
-  props,
-}) => {
+const ReorderableListItem = ({ value, isDragged, props }) => {
   const highlightStyles = {
     backgroundColor: "#ebf8ff",
     boxShadow: "-4px 8px 20px 0px #718096",
@@ -37,3 +33,22 @@ export const ReorderableListItem = ({
     </ListItem>
   );
 };
+
+ReorderableListItem.propTypes = {
+  value: PropTypes.element.isRequired,
+  index: PropTypes.number,
+  isDragged: PropTypes.bool,
+  isSelected: PropTypes.bool,
+  isOutOfBounds: PropTypes.bool,
+  props: PropTypes.shape({
+    key: PropTypes.number,
+    tabIndex: PropTypes.number,
+    "aria-roledescription": PropTypes.string,
+    onKeyDown: PropTypes.func,
+    onWheel: PropTypes.func,
+    style: PropTypes.object,
+    ref: PropTypes.any.isRequired,
+  }).isRequired,
+};
+
+export { ReorderableListItem };
--- src/components/reorderable-list.js
+++ src/components/reorderable-list.js
@@ -1,8 +1,19 @@
 import * as React from "react";
 import { List } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
-export const ReorderableList = ({ children, isDragged, props: { ref } }) => (
-  <List as="ol" ref={ref} isDragged={isDragged}>
+const ReorderableList = ({ children, props: { ref } }) => (
+  <List as="ol" ref={ref}>
     {children}
   </List>
 );
+
+ReorderableList.propTypes = {
+  children: PropTypes.element.isRequired,
+  isDragged: PropTypes.bool,
+  props: PropTypes.shape({
+    ref: PropTypes.any.isRequired,
+  }).isRequired,
+};
+
+export { ReorderableList };
